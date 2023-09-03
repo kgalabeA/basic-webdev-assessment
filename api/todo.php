baf378a5ffde23704f0c69c2524a59bffcac0495
Kgalabe Mogodi <mogodi.albert@gmail.com>
2023-09-03 08:46:04 +0200

    app.js update and todo

--- api/todo.php
+++ api/todo.php
@@ -27,13 +27,77 @@ try {
             }
             break;
         case 'POST':
-            //implement your code here
+            $requestData = json_decode($body);
+            if ($requestData && isset($requestData->title)) {
+                $newTodo = new Todo(
+                    uniqid(),
+                    $requestData->title,
+                    isset($requestData->description) ? $requestData->description : '',
+                    isset($requestData->done) ? $requestData->done : false
+                );
+                if ($controller->create($newTodo)) {
+                    http_response_code(201);
+                    die(json_encode(['message' => 'Todo was created successfully']));
+                } else {
+                    http_response_code(500);
+                    die(json_encode(['message' => 'Failed to create todo']));
+                }
+            } else {
+                http_response_code(400); 
+                die(json_encode(['message' => 'Invalid data request']));
+            }
             break;
         case 'PUT':
-            //implement your code here
+            if ($path[$pathCount - 2] == 'todo' && isset($path[$pathCount - 1]) && strlen($path[$pathCount - 1])) {
+                $id = $path[$pathCount - 1];
+                $todoToUpdate = $controller->load($id);
+                if ($todoToUpdate) {
+                    $requestData = json_decode($body);
+                    if ($requestData && isset($requestData->title)) {
+                        $todoToUpdate->setTitle($requestData->title);
+                        $todoToUpdate->setDescription(isset($requestData->description) ? $requestData->description : '');
+                        $todoToUpdate->setDone(isset($requestData->done) ? $requestData->done : false);
+
+                        if ($controller->update($id, $todoToUpdate)) {
+                            http_response_code(200); 
+                            die(json_encode(['message' => 'Todo was updated successfully']));
+                        } else {
+                            http_response_code(500);
+                            die(json_encode(['message' => 'Failed to update todo']));
+                        }
+                    } else {
+                        http_response_code(400); 
+                        die(json_encode(['message' => 'Invalid data request']));
+                    }
+                } else {
+                    http_response_code(404);
+                    die(json_encode(['message' => 'Todo was not found']));
+                }
+            } else {
+                http_response_code(400); 
+                die(json_encode(['message' => 'Invalid data request']));
+            }
             break;
         case 'DELETE':
-            //implement your code here
+            if ($path[$pathCount - 2] == 'todo' && isset($path[$pathCount - 1]) && strlen($path[$pathCount - 1])) {
+                $id = $path[$pathCount - 1];
+                $todoToDelete = $controller->load($id);
+                if ($todoToDelete) {
+                    if ($controller->delete($id)) {
+                        http_response_code(204); 
+                        die();
+                    } else {
+                        http_response_code(500);
+                        die(json_encode(['message' => 'Failed to delete todo']));
+                    }
+                } else {
+                    http_response_code(404); 
+                    die(json_encode(['message' => 'Todo was not found']));
+                }
+            } else {
+                http_response_code(400);
+                die(json_encode(['message' => 'Invalid data request']));
+            }
             break;
         default:
             http_response_code(501);
--- app.js
+++ app.js
@@ -1,27 +1,65 @@
 function putTodo(todo) {
-    // implement your code here
-    console.log("calling putTodo");
-    console.log(todo);
+  fetch(window.location.href + "api/todo/" + todo.id, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(todo),
+  })
+    .then((response) => {
+      if (response.ok) {
+        console.log("TODO was updated successfully");
+      } else {
+        console.error("Failed to update TODO");
+      }
+    })
+    .catch((error) => console.error("Error:", error));
+  console.log("calling putTodo");
+  console.log(todo);
 }
 
 function postTodo(todo) {
-    // implement your code here
-    console.log("calling postTodo");
-    console.log(todo);
+  fetch(window.location.href + "api/todo", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(todo),
+  })
+    .then((response) => {
+      if (response.ok) {
+        console.log("TODO was created successfully");
+      } else {
+        console.error("Failed to create TODO");
+      }
+    })
+    .catch((error) => console.error("Error:", error));
+  console.log("calling postTodo");
+  console.log(todo);
 }
 
 function deleteTodo(todo) {
-    // implement your code here
-    console.log("calling deleteTodo");
-    console.log(todo);
+  fetch(window.location.href + "api/todo/" + todo.id, {
+    method: "DELETE",
+  })
+    .then((response) => {
+      if (response.ok) {
+        console.log("TODO deleted successfully");
+      } else {
+        console.error("Failed to delete TODO");
+      }
+    })
+    .catch((error) => console.error("Error:", error));
+  console.log("calling deleteTodo");
+  console.log(todo);
 }
 
 // example using the FETCH API to do a GET request
 function getTodos() {
-    fetch(window.location.href + 'api/todo')
-    .then(response => response.json())
-    .then(json => drawTodos(json))
-    .catch(error => showToastMessage('Failed to retrieve todos...'));
+  fetch(window.location.href + "api/todo")
+    .then((response) => response.json())
+    .then((json) => drawTodos(json))
+    .catch((error) => showToastMessage("Failed to retrieve todos..."));
 }
 
 getTodos();
